07754363ac5dbb3f40a99835151a945dd7b6a145
Seryy <s180707@mail.ru>
2020-06-28 18:54:49 +0300

    creation project

---
+++ src/main/java/ru/v/r/logisticplatform/LogisticPlatformApplication.java
@@ -0,0 +1,13 @@
+package ru.v.r.logisticplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/v/r/logisticplatform/LogisticPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.v.r.logisticplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
